6344f0439d385c0503430e0850824ddc9343e18f
Enes Sarıyıldız <ens.sryldz@gmail.com>
2022-08-01 15:36:37 +0300

    Update
    Windows form iphone hesap makinesi klon

--- HesapMakinesi/HesapMakinesi/Form1.Designer.cs
+++ HesapMakinesi/HesapMakinesi/Form1.Designer.cs
@@ -49,6 +49,12 @@
             this.FiveButton = new System.Windows.Forms.Button();
             this.NineButton = new System.Windows.Forms.Button();
             this.SixButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ScreenLabel
@@ -72,6 +78,7 @@
             this.acButton.TabIndex = 1;
             this.acButton.Text = "AC";
             this.acButton.UseVisualStyleBackColor = true;
+            this.acButton.Click += new System.EventHandler(this.acButton_Click);
             // 
             // NegativeAndPozitiveButton
             // 
@@ -145,6 +152,7 @@
             // SevenButton
             // 
             this.SevenButton.BackColor = System.Drawing.SystemColors.GrayText;
+            this.SevenButton.ForeColor = System.Drawing.SystemColors.Control;
             this.SevenButton.Location = new System.Drawing.Point(13, 211);
             this.SevenButton.Name = "SevenButton";
             this.SevenButton.Size = new System.Drawing.Size(94, 50);
@@ -156,6 +164,7 @@
             // FourButton
             // 
             this.FourButton.BackColor = System.Drawing.SystemColors.GrayText;
+            this.FourButton.ForeColor = System.Drawing.SystemColors.Control;
             this.FourButton.Location = new System.Drawing.Point(13, 267);
             this.FourButton.Name = "FourButton";
             this.FourButton.Size = new System.Drawing.Size(94, 50);
@@ -167,6 +176,7 @@
             // OneButton
             // 
             this.OneButton.BackColor = System.Drawing.SystemColors.GrayText;
+            this.OneButton.ForeColor = System.Drawing.SystemColors.Control;
             this.OneButton.Location = new System.Drawing.Point(13, 323);
             this.OneButton.Name = "OneButton";
             this.OneButton.Size = new System.Drawing.Size(94, 50);
@@ -178,6 +188,7 @@
             // ZeroButton
             // 
             this.ZeroButton.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ZeroButton.ForeColor = System.Drawing.SystemColors.Control;
             this.ZeroButton.Location = new System.Drawing.Point(12, 379);
             this.ZeroButton.Name = "ZeroButton";
             this.ZeroButton.Size = new System.Drawing.Size(194, 50);
@@ -189,6 +200,7 @@
             // TwoButton
             // 
             this.TwoButton.BackColor = System.Drawing.SystemColors.GrayText;
+            this.TwoButton.ForeColor = System.Drawing.SystemColors.Control;
             this.TwoButton.Location = new System.Drawing.Point(113, 323);
             this.TwoButton.Name = "TwoButton";
             this.TwoButton.Size = new System.Drawing.Size(94, 50);
@@ -200,6 +212,7 @@
             // ThreeButton
             // 
             this.ThreeButton.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ThreeButton.ForeColor = System.Drawing.SystemColors.Control;
             this.ThreeButton.Location = new System.Drawing.Point(213, 323);
             this.ThreeButton.Name = "ThreeButton";
             this.ThreeButton.Size = new System.Drawing.Size(94, 50);
@@ -221,6 +234,7 @@
             // EıghtButton
             // 
             this.EıghtButton.BackColor = System.Drawing.SystemColors.GrayText;
+            this.EıghtButton.ForeColor = System.Drawing.SystemColors.Control;
             this.EıghtButton.Location = new System.Drawing.Point(112, 211);
             this.EıghtButton.Name = "EıghtButton";
             this.EıghtButton.Size = new System.Drawing.Size(94, 50);
@@ -232,6 +246,7 @@
             // FiveButton
             // 
             this.FiveButton.BackColor = System.Drawing.SystemColors.GrayText;
+            this.FiveButton.ForeColor = System.Drawing.SystemColors.Control;
             this.FiveButton.Location = new System.Drawing.Point(113, 267);
             this.FiveButton.Name = "FiveButton";
             this.FiveButton.Size = new System.Drawing.Size(94, 50);
@@ -243,6 +258,7 @@
             // NineButton
             // 
             this.NineButton.BackColor = System.Drawing.SystemColors.GrayText;
+            this.NineButton.ForeColor = System.Drawing.SystemColors.Control;
             this.NineButton.Location = new System.Drawing.Point(212, 211);
             this.NineButton.Name = "NineButton";
             this.NineButton.Size = new System.Drawing.Size(94, 50);
@@ -254,6 +270,7 @@
             // SixButton
             // 
             this.SixButton.BackColor = System.Drawing.SystemColors.GrayText;
+            this.SixButton.ForeColor = System.Drawing.SystemColors.Control;
             this.SixButton.Location = new System.Drawing.Point(212, 267);
             this.SixButton.Name = "SixButton";
             this.SixButton.Size = new System.Drawing.Size(94, 50);
@@ -261,6 +278,77 @@
             this.SixButton.Text = "6";
             this.SixButton.UseVisualStyleBackColor = false;
             this.SixButton.Click += new System.EventHandler(this.SixButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(312, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 50);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "%";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkOrange;
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(312, 211);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 50);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkOrange;
+            this.button3.ForeColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(312, 267);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 50);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DarkOrange;
+            this.button4.ForeColor = System.Drawing.SystemColors.Control;
+            this.button4.Location = new System.Drawing.Point(313, 323);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 50);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "+";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.DarkOrange;
+            this.button5.ForeColor = System.Drawing.SystemColors.Control;
+            this.button5.Location = new System.Drawing.Point(313, 379);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 50);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "=";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.GrayText;
+            this.button6.ForeColor = System.Drawing.SystemColors.Control;
+            this.button6.Location = new System.Drawing.Point(213, 379);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(94, 50);
+            this.button6.TabIndex = 15;
+            this.button6.Text = ",";
+            this.button6.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -272,16 +360,22 @@
             this.Controls.Add(this.NineButton);
             this.Controls.Add(this.FiveButton);
             this.Controls.Add(this.EıghtButton);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button15);
             this.Controls.Add(this.ThreeButton);
             this.Controls.Add(this.TwoButton);
             this.Controls.Add(this.ZeroButton);
             this.Controls.Add(this.OneButton);
             this.Controls.Add(this.FourButton);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.SevenButton);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.EqualButton);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.PozitiveButton);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.NegativeButton);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.ImpactButton);
             this.Controls.Add(this.DivideButton);
             this.Controls.Add(this.ClearButton);
@@ -320,5 +414,11 @@
         private Button FiveButton;
         private Button NineButton;
         private Button SixButton;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
     }
 }
--- HesapMakinesi/HesapMakinesi/Form1.cs
+++ HesapMakinesi/HesapMakinesi/Form1.cs
@@ -2,6 +2,10 @@ namespace HesapMakinesi
 {
     public partial class Form1 : Form
     {
+        private char _islem;
+        bool _ekranTemizlenekcekMi;
+        private int _ilkSayi;
+
         public Form1()
         {
             InitializeComponent();
@@ -9,77 +13,219 @@ namespace HesapMakinesi
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            if (_ekranTemizlenekcekMi)
+            {
+                ScreenLabel.Text = "";
+                _ekranTemizlenekcekMi = false;
+            }
+            if (ScreenLabel.Text == "0") ScreenLabel.Text = "";
+            ScreenLabel.Text += "1";
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-
+            if (_ekranTemizlenekcekMi)
+            {
+                ScreenLabel.Text = "";
+                _ekranTemizlenekcekMi = false;
+            }
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
+            if (_ekranTemizlenekcekMi)
+            {
+                ScreenLabel.Text = "";
+                _ekranTemizlenekcekMi = false;
+            }
 
+
+            _islem = '*';
+            _ekranTemizlenekcekMi = true;
+            _ilkSayi = Convert.ToInt32(ScreenLabel.Text);
         }
 
         private void button14_Click(object sender, EventArgs e)
         {
+            if (_ekranTemizlenekcekMi) ScreenLabel.Text = "";
             if (ScreenLabel.Text == "0") ScreenLabel.Text = "";
             ScreenLabel.Text += "3";
         }
 
         private void OneButton_Click(object sender, EventArgs e)
         {
+            if (_ekranTemizlenekcekMi)
+            {
+                ScreenLabel.Text = "";
+                _ekranTemizlenekcekMi = false;
+            }
             if (ScreenLabel.Text == "0") ScreenLabel.Text = "";
             ScreenLabel.Text += "1";
         }
 
         private void TwoButton_Click(object sender, EventArgs e)
         {
+            if (_ekranTemizlenekcekMi)
+            {
+                ScreenLabel.Text = "";
+                _ekranTemizlenekcekMi = false;
+            }
             if (ScreenLabel.Text == "0") ScreenLabel.Text = "";
             ScreenLabel.Text += "2";
         }
 
         private void FourButton_Click(object sender, EventArgs e)
         {
+            if (_ekranTemizlenekcekMi)
+            {
+                ScreenLabel.Text = "";
+                _ekranTemizlenekcekMi = false;
+            }
             if (ScreenLabel.Text == "0") ScreenLabel.Text = "";
             ScreenLabel.Text += "4";
         }
 
         private void FiveButton_Click(object sender, EventArgs e)
         {
+            if (_ekranTemizlenekcekMi)
+            {
+                ScreenLabel.Text = "";
+                _ekranTemizlenekcekMi = false;
+            }
             if (ScreenLabel.Text == "0") ScreenLabel.Text = "";
             ScreenLabel.Text += "5";
         }
 
         private void SixButton_Click(object sender, EventArgs e)
         {
+            if (_ekranTemizlenekcekMi)
+            {
+                ScreenLabel.Text = "";
+                _ekranTemizlenekcekMi = false;
+            }
             if (ScreenLabel.Text == "0") ScreenLabel.Text = "";
             ScreenLabel.Text += "6";
         }
 
         private void NineButton_Click(object sender, EventArgs e)
         {
+            if (_ekranTemizlenekcekMi)
+            {
+                ScreenLabel.Text = "";
+                _ekranTemizlenekcekMi = false;
+            }
             if (ScreenLabel.Text == "0") ScreenLabel.Text = "";
             ScreenLabel.Text += "9";
         }
 
         private void EýghtButton_Click(object sender, EventArgs e)
         {
+            if (_ekranTemizlenekcekMi)
+            {
+                ScreenLabel.Text = "";
+                _ekranTemizlenekcekMi = false;
+            }
             if (ScreenLabel.Text == "0") ScreenLabel.Text = "";
             ScreenLabel.Text += "8";
         }
 
         private void SevenButton_Click(object sender, EventArgs e)
         {
+            if (_ekranTemizlenekcekMi)
+            {
+                ScreenLabel.Text = "";
+                _ekranTemizlenekcekMi = false;
+            }
             if (ScreenLabel.Text == "0") ScreenLabel.Text = "";
             ScreenLabel.Text += "7";
         }
 
         private void ZeroButton_Click(object sender, EventArgs e)
         {
+            if (_ekranTemizlenekcekMi)
+            {
+                ScreenLabel.Text = "";
+                _ekranTemizlenekcekMi = false;
+            }
             if (ScreenLabel.Text == "0") ScreenLabel.Text = "";
             ScreenLabel.Text += "0";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+
+
+
+            if (_ekranTemizlenekcekMi)
+            {
+                ScreenLabel.Text = "";
+                _ekranTemizlenekcekMi = false;
+            }
+
+
+            _islem = '+';
+            _ekranTemizlenekcekMi = true;
+            _ilkSayi = Convert.ToInt32(ScreenLabel.Text);
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            int ikinciSayi = Convert.ToInt32(ScreenLabel.Text);
+            int sonuc;
+
+            switch (_islem)
+            {
+                case '+':
+                    sonuc = _ilkSayi + ikinciSayi;
+                    break;
+                case '-':
+                    sonuc = _ilkSayi - ikinciSayi;
+                    break;
+                case '*':
+                    sonuc = _ilkSayi * ikinciSayi;
+                    break;
+                case '/':
+                    sonuc = _ilkSayi / ikinciSayi;
+                    break;
+                default:
+                    sonuc = 0;
+                    break;
+            }
+            ScreenLabel.Text = Convert.ToString(sonuc);
+        }
+
+        private void acButton_Click(object sender, EventArgs e)
+        {
+            ScreenLabel.Text = "0";
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (_ekranTemizlenekcekMi)
+            {
+                ScreenLabel.Text = "";
+                _ekranTemizlenekcekMi = false;
+            }
+
+
+            _islem = '/';
+            _ekranTemizlenekcekMi = true;
+            _ilkSayi = Convert.ToInt32(ScreenLabel.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (_ekranTemizlenekcekMi)
+            {
+                ScreenLabel.Text = "";
+                _ekranTemizlenekcekMi = false;
+            }
+
+
+            _islem = '-';
+            _ekranTemizlenekcekMi = true;
+            _ilkSayi = Convert.ToInt32(ScreenLabel.Text);
         }
     }
 }
